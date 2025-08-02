24cff97fa2e04e767cd256d26e3dfc405e6fb2e8
krishbains <krishbains@gmail.com>
2025-08-02 19:25:34 +0530

    Update socket.ts

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,6 +2,7 @@
 import { io } from "socket.io-client"
 
 // This runs in the browser, not server
-const socket = io("http://localhost:4000") // Same port as your backend
+// const socket = io("http://localhost:4000") // Same port as your backend
+const socket = io('https://ghost-quizzer.onrender.com/')
 
 export default socket
