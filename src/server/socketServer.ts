6f83842f1d3792e4ef63846c29c85aeb2fac4416
krishbains <krishbains@gmail.com>
2025-08-02 20:06:33 +0530

    fixing more issues to deploy the server

--- src/server/socketServer.ts
+++ src/server/socketServer.ts
@@ -184,6 +184,8 @@ io.on("connection", (socket) => {
   })
 })
 
-server.listen(4000, () => {
-  console.log("🚀 Socket.IO server running on port 4000")
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`🚀 Socket.IO server running on port ${PORT}`)
 })
